7e620e56ec45a39c2d6da3f9f60695461261f0ea
4Source <38220764+4Source@users.noreply.github.com>
2024-03-15 19:11:55 +0100

    Added function fetchManifest + add regex check

--- src/util/GitHub.ts
+++ src/util/GitHub.ts
@@ -2,7 +2,9 @@
  * Credits: https://github.com/TfTHacker/obsidian42-brat
  */
 
-import { request } from "obsidian";
+import { PluginManifest, request } from "obsidian";
+
+const repositoryRegEx = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}\/[A-Za-z0-9_.-]+$/gi;
 
 export interface CommunityPlugin {
     id: string;
@@ -49,17 +51,17 @@ export type File = {
  * Fetch all community plugin entrys.
  * @returns A list of community plugins
  */
-export async function fetchCommmunityPluginList(): Promise<CommunityPlugin[]> {
-    const pluginListUrl = `https://raw.githubusercontent.com/obsidianmd/obsidian-releases/HEAD/community-plugins.json`;
+export async function fetchCommmunityPluginList(): Promise<CommunityPlugin[] | undefined> {
+    const URL = `https://raw.githubusercontent.com/obsidianmd/obsidian-releases/HEAD/community-plugins.json`;
     try {
-        const response = await request({ url: pluginListUrl });
-        return response === '404: Not Found' ? [] : (
-            ((await JSON.parse(response)) as CommunityPlugin[])
-        );
+        // Do a request to the url
+        const response = await request({ url: URL });
+
+        // Process the response
+        return (await JSON.parse(response)) as CommunityPlugin[];
     } catch (e) {
         (e as Error).message = 'Failed to fetch community plugin list! ' + (e as Error).message;
         console.error(e);
-        return [];
     }
 };
 
@@ -68,14 +70,17 @@ export async function fetchCommmunityPluginList(): Promise<CommunityPlugin[]> {
  * @param repository The <user>/<repo> of the plugin
  * @returns A list of all releases
  */
-export async function fetchReleases(repository: string): Promise<Partial<Release>[]> {
+export async function fetchReleases(repository: string): Promise<Partial<Release>[] | undefined> {
+    const URL = `https://api.github.com/repos/${repository}/releases`;
     try {
-        const URL = `https://api.github.com/repos/${repository}/releases`;
-        const response = await request({
-            url: URL
-        });
+        if(!repositoryRegEx.test(repository)) {
+            throw Error('Repository string do not match the pattern!');
+        }
+        // Do a request to the url
+        const response = await request({ url: URL });
+        // Process the response
         const data = await JSON.parse(response);
-        let releases = data.map((value: Release) => {
+        const releases = data.map((value: Release) => {
             return {
                 tag_name: value.tag_name,
                 prerelease: value.prerelease
@@ -86,23 +91,46 @@ export async function fetchReleases(repository: string): Promise<Partial<Release
     catch (e) {
         (e as Error).message = 'Failed to fetch releases for plugin! ' + (e as Error).message;
         console.error(e);
-        return [];
+    }
+}
+
+/**
+ * Fetch the manifest for a plugin
+ * @param repository The <user>/<repo> of the plugin
+ * @param tag_name The name of the tag associated with a release. Required if a specific manifest version is needed.
+ * @returns The plugin manifest object
+ */
+export async function fetchManifest(repository: string, tag_name?: string): Promise<PluginManifest | undefined> {
+    const URL = `https://raw.githubusercontent.com/${repository}/${tag_name ? tag_name : 'HEAD'}/manifest.json`;
+    try {
+        if(!repositoryRegEx.test(repository)) {
+            throw Error('Repository string do not match the pattern!');
+        }
+        // Do a request to the url
+        const response = await request({ url: URL });
+
+        // Process the response
+        return (await JSON.parse(response)) as PluginManifest;
+    } catch (e) {
+        (e as Error).message = 'Failed to fetch the manifest for plugin! ' + (e as Error).message;
+        console.error(e);
     }
 }
 
 /**
  * Downloads the assets from a release from GitHub
  * @param repository The <user>/<repo> of the plugin
- * @param tag_name The name of the tag belongs the the release
+ * @param tag_name The name of the tag associated with a release.
  * @returns The Assets attached to the release as string
  * @todo Checksum 
  */
 export async function downloadReleaseAssets(repository: string, tag_name: string): Promise<File[] | undefined> {
+    const URL = `https://api.github.com/repos/${repository}/releases/tags/${tag_name}`;
     try {
-        const URL = `https://api.github.com/repos/${repository}/releases/tags/${tag_name}`;
-        const response = await request({
-            url: URL
-        });
+        if(!repositoryRegEx.test(repository)) {
+            throw Error('Repository string do not match the pattern!');
+        }
+        const response = await request({ url: URL });
         const data = await JSON.parse(response);
         let assets: Asset[] = data.assets;
 

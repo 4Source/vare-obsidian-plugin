165ee0b604ab535b92ca21989761a1d0b0a29676
4Source <38220764+4Source@users.noreply.github.com>
2024-03-17 16:22:28 +0100

    Add installed and saved plugins to list

--- src/constants.ts
+++ src/constants.ts
@@ -7,3 +7,5 @@ export const ICON_REMOVE = 'trash-2';
 export const ICON_SAVE = 'save';
 export const ICON_INSTALL = 'download';
 export const ICON_GITHUB = 'github';
+export const ICON_FIX = 'wrench';
+export const ICON_RESET = 'loader';
--- src/settings/SettingsTab.ts
+++ src/settings/SettingsTab.ts
@@ -1,9 +1,9 @@
 import { App, PluginSettingTab, Setting } from 'obsidian';
-import { ICON_ADD, ICON_GITHUB, ICON_INSTALL, ICON_RELOAD, ICON_REMOVE } from 'src/constants';
+import { ICON_ADD, ICON_FIX, ICON_GITHUB, ICON_INSTALL, ICON_RELOAD, ICON_RESET } from 'src/constants';
 import VarePlugin from 'src/main';
 import { PluginData, PluginInfo } from './SettingsInterface';
 import { PluginDataModal } from 'src/modals/PluginDataModal';
-import { fetchManifest } from 'src/util/GitHub';
+import { fetchCommmunityPluginList, fetchManifest, fetchReleases } from 'src/util/GitHub';
 
 export class VareSettingTab extends PluginSettingTab {
 	plugin: VarePlugin;
@@ -13,136 +13,184 @@ export class VareSettingTab extends PluginSettingTab {
 		super(app, plugin);
 		this.plugin = plugin;
 
-		this.pluginsList = [];
-		this.pluginsList.push({
-			author: 'Test',
-			id: 'test',
-			name: 'Test',
-			releases: [
-				{
-					tag_name: '0.0.1',
-				},
-				{
-					tag_name: '0.0.0',
-				},
-			],
-			repo: '',
-			version: '0.0.0',
+		const manifests = Object.entries(structuredClone(this.plugin.app.plugins.manifests));
+		const pluginData = Object.entries(this.plugin.settings.plugins);
+		this.pluginsList = manifests.map(manifest => {
+			const info: PluginInfo = { ...(manifest[1]), repo: '', releases: [] };
+			const data = pluginData.filter(data => data[0] === manifest[0])[0];
+			if (!data) {
+				return info;
+			}
+			return Object.assign(info, data[1]);
 		});
 	}
 
-	display(): void {
+	async display(): Promise<void> {
 		const { containerEl } = this;
 
 		containerEl.empty();
 
-		// Heading for Profiles
-		new Setting(containerEl)
-			.setHeading()
-			.setName('Plugins')
-			.addExtraButton(button => button
-				.setIcon(ICON_ADD)
-				.setTooltip('Add unlisted plugin')
-				.onClick(() => {
-					// Open plugin modal
-					new PluginDataModal(this.plugin, result => {
-						this.pluginsList.push(result);
-						this.display();
-					})
-						.open();
-				}))
-			.addExtraButton(button => button
-				.setIcon(ICON_RELOAD)
-				.setTooltip('Reload plugins')
-				.onClick(() => {
-					// Reload plugins
-					this.display();
-				}));
+		// Get the releases for plugins
+		const communityList = await fetchCommmunityPluginList();
+		Promise.all(this.pluginsList.map(async value => {
+			if (value.releases.length <= 0) {
+				// Get repo from community plugin list if there is an entry
+				if (value.repo === '') {
+					const community = communityList?.find(community => community.id === value.id);
+					if (!community) {
+						return;
+					}
+					value.repo = community.repo;
+				}
+				// Fetch releases from github
+				const releases = await fetchReleases(value.repo);
+				if (!releases) {
+					return;
+				}
+				value.releases = releases;
+			}
+		}))
+			.then(() => {
+				// Heading for Profiles
+				new Setting(containerEl)
+					.setHeading()
+					.setName('Plugins')
+					.addExtraButton(button => button
+						.setIcon(ICON_ADD)
+						.setTooltip('Add unlisted plugin')
+						.onClick(() => {
+							// Open plugin modal
+							new PluginDataModal(this.plugin, result => {
+								this.pluginsList.push(result);
+								this.display();
+							})
+								.open();
+						}))
+					.addExtraButton(button => button
+						.setIcon(ICON_RELOAD)
+						.setTooltip('Reload plugins')
+						.onClick(() => {
+							// Reload plugins
+							this.display();
+						}));
 
-		this.pluginsList.forEach(plugin => {
-			let versions = {};
-			if (plugin.releases.length > 0) {
-				plugin.releases.forEach(element => {
-					if (element.tag_name) {
-						const key = element.tag_name;
-						const value = element.tag_name;
-						versions = Object.assign(versions, { [key]: value });
+				this.pluginsList.forEach(plugin => {
+					// Build dropdown options with releasees
+					let versions = {};
+					if (plugin.releases.length > 0) {
+						plugin.releases.forEach(element => {
+							if (element.tag_name) {
+								const key = element.tag_name;
+								const value = element.tag_name;
+								versions = Object.assign(versions, { [key]: value });
+							}
+						});
+					}
+
+					// Missing information
+					let trouble = false;
+
+					// Plugin Info
+					const settings = new Setting(containerEl.createEl('div', { cls: 'plugins-container' }))
+						.setName(plugin.name)
+						.setDesc(createFragment((fragment) => {
+							fragment.append(`Installed version: ${plugin.version}`, fragment.createEl('br'), `Author: ${plugin.author}`);
+						}));
+
+					// GitHub link button
+					if (plugin.repo) {
+						settings.addExtraButton(button => button
+							.setIcon(ICON_GITHUB)
+							.setTooltip('Open at GitHub')
+							.onClick(async () => {
+								self.open(`https://github.com/${plugin.repo}`);
+							}));
+					}
+					else {
+						trouble = true;
+					}
+
+					// Reset version
+					settings.addExtraButton(button => button
+						.setIcon(ICON_RESET)
+						.setTooltip('Reset version')
+						.onClick(async () => {
+							delete plugin.targetVersion;
+							this.display();
+						}));
+
+					// Version dropdown
+					if (plugin.releases.length > 0) {
+						settings.addDropdown(dropdown => dropdown
+							.addOptions(versions)
+							.setValue(plugin.targetVersion || '')
+							.onChange(value => {
+								plugin.targetVersion = value;
+								this.display();
+							}));
+					}
+					else {
+						trouble = true;
+					}
+
+					// Trouble shooting plugin
+					if (trouble) {
+						settings.addButton(button => button
+							.setIcon(ICON_FIX)
+							.setTooltip('Troubleshoot plugin.')
+							.setWarning()
+							.onClick(() => {
+								// open modal
+							}));
+					}
+
+					if (plugin.targetVersion && plugin.version !== plugin.targetVersion) {
+						settings.addExtraButton(button => button
+							.setIcon(ICON_INSTALL)
+							.setTooltip('Install version')
+							.onClick(async () => {
+								try {
+									// Fetch the manifest from GitHub
+									const manifest = await fetchManifest(plugin.repo, plugin.targetVersion);
+									if (!manifest) {
+										throw Error('No manifest found for this plugin!');
+									}
+									// Ensure contains dir
+									if (!manifest.dir) {
+										manifest.dir = plugin.id;
+									}
+									// Get the version that should be installed
+									const version = plugin.targetVersion || manifest.version;
+									if (!version) {
+										throw Error('Manifest do not contain a version!');
+									}
+									// Install plugin
+									await this.plugin.app.plugins.installPlugin(plugin.repo, version, manifest);
+									// Update manifest
+									const installed = this.plugin.app.plugins.manifests[plugin.id];
+									if (!installed) {
+										throw Error('Installation failed!');
+									}
+									plugin.version = installed.version;
+
+									// Update and save Settings
+									const data: PluginData = {
+										id: plugin.id,
+										repo: plugin.repo,
+										targetVersion: plugin.targetVersion,
+									};
+									this.plugin.settings.plugins[plugin.id] = data;
+									await this.plugin.saveSettings();
+
+									this.display();
+								}
+								catch (e) {
+									(e as Error).message = 'Failed to install plugin! ' + (e as Error).message;
+									console.error(e);
+								}
+							}));
 					}
 				});
-			}
-
-			const settings = new Setting(containerEl.createEl('div', { cls: 'plugins-container' }))
-				.setName(plugin.name)
-				.setDesc(createFragment((fragment) => {
-					fragment.append(`Installed version: ${plugin.version}`, fragment.createEl('br'), `Author: ${plugin.author}`);
-				}))
-				.addExtraButton(button => button
-					.setIcon(ICON_GITHUB)
-					.setTooltip('Open at GitHub')
-					.onClick(async () => {
-						// Remove plugin
-					}))
-				.addExtraButton(button => button
-					.setIcon(ICON_REMOVE)
-					.setTooltip('Remove')
-					.onClick(async () => {
-						// Remove plugin
-					}))
-				.addDropdown(dropdown => dropdown
-					.addOptions(versions)
-					.setValue(plugin.targetVersion || plugin.version)
-					.onChange(value => {
-						plugin.targetVersion = value;
-						this.display();
-					}));
-
-			if (plugin.targetVersion && plugin.version !== plugin.targetVersion) {
-				settings
-					.addExtraButton(button => button
-						.setIcon(ICON_INSTALL)
-						.setTooltip('Install version')
-						.onClick(async () => {
-							try {
-								// Fetch the manifest from GitHub
-								const manifest = await fetchManifest(plugin.repo, plugin.targetVersion);
-								if (!manifest) {
-									throw Error('No manifest found for this plugin!');
-								}
-								// Ensure contains dir
-								if (!manifest.dir) {
-									manifest.dir = plugin.id;
-								}
-								// Get the version that should be installed
-								const version = plugin.targetVersion || manifest.version;
-								if (!version) {
-									throw Error('Manifest do not contain a version!');
-								}
-								// Install plugin
-								await this.plugin.app.plugins.installPlugin(plugin.repo, version, manifest);
-								// Update manifest
-								const installed = this.plugin.app.plugins.manifests[plugin.id];
-								if (!installed) {
-									throw Error('Installation failed!');
-								}
-								plugin.version = installed.version;
-
-								// Update and save Settings
-								const data: PluginData = {
-									id: plugin.id,
-									repo: plugin.repo,
-									targetVersion: plugin.targetVersion,
-								};
-								this.plugin.settings.plugins[plugin.id] = data;
-								await this.plugin.saveSettings();
-
-								this.display();
-							}
-							catch (e) {
-								(e as Error).message = 'Failed to install plugin! ' + (e as Error).message;
-								console.error(e);
-							}
-						}));
-			}
-		});
+			});
 	}
 }

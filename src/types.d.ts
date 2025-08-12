7e5fb13be6375a200e272a2288257032a8807be6
4Source <38220764+4Source@users.noreply.github.com>
2025-08-12 22:12:15 +0200

    Fix missing typing

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,6 +3,7 @@ import { } from 'obsidian';
 declare module 'obsidian' {
 	interface Plugins {
 		manifests: Record<string, PluginManifest>;
+		installPlugin(repo: string, version: string, manifest: PluginManifest): Promise<void>;
 	}
 
 	interface App {
